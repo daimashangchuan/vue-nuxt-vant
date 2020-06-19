18a6eaa15f2c374c1c94630e69f3af9004e4090f
sunjiwei <sunjiwei@fulan.com.cn>
2020-06-19 13:41:06 +0800

    添加ajax请求方法

--- plugins/axios.js
+++ plugins/axios.js
@@ -19,18 +19,19 @@ export default ({ store }) => {
   // 跨域访问需要发送cookie
   axios.defaults.withCredentials = true;
   // post 请求需要 Content-Type
-  axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+  axios.defaults.headers.post["Content-Type"] =
+    "application/x-www-form-urlencoded;charset=UTF-8";
 
   // 请求 request 拦截器，在这里，我们给每个请求都加 token
   axios.interceptors.request.use(
     request => {
-      request.headers['X-SZK-Channel'] = 'h5';
-      request.headers['X-SZK-Token'] = '';
-      request.headers['X-SZK-UUID'] = '';
+      request.headers["X-SZK-Channel"] = "h5";
+      request.headers["X-SZK-Token"] = "";
+      request.headers["X-SZK-UUID"] = "";
 
       // 动画效果
       const loadingFlag = request.data.NoLoading;
-      if(!loadingFlag) {  
+      if (!loadingFlag) {
         store.dispatch("AppStore/setLoading", true);
       }
       return request;
@@ -43,15 +44,45 @@ export default ({ store }) => {
   // 响应 response 拦截器
   axios.interceptors.response.use(
     response => {
-      store.dispatch('AppStore/setLoading', false);
+      store.dispatch("AppStore/setLoading", false);
       return response.data.result;
     },
     //-- 对响应错误做点什么
     error => {
-      store.dispatch('AppStore/setLoading', false);
+      store.dispatch("AppStore/setLoading", false);
       return Promise.reject(error);
     }
   );
   return axios;
 };
 
+export function ajaxRequest(url, method, data, callback) {
+  var xhr;
+  try {
+    // Firefox, Opera 8.0+, Safari
+    xhr = new XMLHttpRequest();
+  } catch (e) {
+    try {
+      // Internet Explorer
+      xhr = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+      try {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+      } catch (e) {}
+    }
+  }
+  xhr.open(method, url, true);
+  //如果是POST请求方式，设置请求首部信息
+  xhr.setRequestHeader("utoken", token);
+  xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+  xhr.send(data);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200 || xhr.status == 304) {
+        var res = xhr.responseText;
+        var data = JSON.parse(res);
+        callback(data);
+      }
+    }
+  };
+}
