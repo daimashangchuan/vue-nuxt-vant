af3b7d98f6327130c9176905348ce869466ef00c
sunjiwei <sunjiwei@fulan.com.cn>
2020-07-10 18:02:45 +0800

    刷脸demo

--- pages/tabFifth/user-media.vue
+++ pages/tabFifth/user-media.vue
@@ -1,63 +1,47 @@
 <template>
-  <div>
-    <!--照片区域-->
-    <div
-      v-for="(urls, index) in imgs "
-      :key="index"
-      style="float: left; margin: 10px ; border: 1px solid #ccc;"
-    >
-      <div style="text-align: right; position: relative;" v-on:click="deleteImg(index)">X</div>
-      <img :src="urls" width="100px" height="100px" />
-    </div>
-    <div
-      style=" width: 100px; height: 100px; background-color: #ccc; border:1px solid #ccc; float: left;  margin: 10px ;"
-      v-on:click="imgClick()"
-    ></div>
+  <div class="user-media">
+    <video id="video" autoplay width="300" height="300" controls></video>
+    <van-button class="video-btn" type="primary" @click.native="upBtnFileFace">刷脸验证</van-button>
     <input
-      style="float: left;  display: none;"
+      class="file"
+      ref="videoFace"
+      name="file"
       type="file"
-      id="uploadFile"
-      accept="image/*"
-      v-on:change="readLocalFile()"
+      accept="video/*"
+      @change="updateFace"
+      style="display: none"
     />
   </div>
 </template>
- 
+
 <script>
 export default {
   data() {
-    return {
-      imgs: []
-    };
   },
   methods: {
-    //删除图片
-    deleteImg: function(index) {
-      this.imgs.splice(index, 1);
-    },
-    //图片click
-    imgClick: function() {
-      document.getElementById("uploadFile").click();
-    },
-    //点击选中图片
-    readLocalFile: function() {
-      var localFile = document.getElementById("uploadFile").files[0];
-      var reader = new FileReader();
-      var content;
-      var current = this;
-      reader.onload = function(event) {
-        content = event.target.result;
-        current.imgs.push(content); //获取图片base64代码
-      };
-      reader.onerror = function(event) {
-        alert("error");
-      };
-      content = reader.readAsDataURL(localFile, "UTF-8");
-      var sss = document.getElementById("uploadFile").value;
-      console.log(sss);
+    updateFace(e) {
+      const file = e.target.files[0] || e.dataTransfer.files[0];
+      if (file) {
+        // 本地预览
+        const reader = new FileReader();
+        const myVideo = document.querySelector("#video");
+        reader.onloadend = evt => {
+          myVideo.src = evt.target.result;
+        };
+        reader.readAsDataURL(file);
+        myVideo.play();
+        // 上传
+        const fd = new FormData();
+        fd.append("file", file);
+        ocrIdCard(fd).then(response => {
+          if (response) {
+            console.log(response);
+          }
+        }).catch(() => {});
+      }
     }
-  },
+  }
 };
 </script>
- 
+
 <style scoped></style>
